a1c7ff322a53c89c76a2870abd1176821a012759
mark-summerfield <mark@qtrac.eu>
2020-03-13 20:02:52 +0000

    Added support for union (& and &=) and intersection (| and |=)

--- src/aaset.d
+++ src/aaset.d
@@ -5,7 +5,7 @@
  * clear, length, iteration, and toString (e.g., for debugging or
  * testing).
  *
- * See the unittest block for examples.
+ * See the unittest block at the end for examples.
  *
  * Authors: Mark Summerfield, mark@qtrac.eu
  * License: Apache 2.0
@@ -65,12 +65,63 @@ struct AAset(T) if (is(int[T])) {
     auto opSlice() const { return set.byKey; }
 
     /**
-     * Supports the `in` operator.
+     * Provides the `in` operator.
      * Params: item to check for membership in the set.
      * Returns: true if the item is in the set, or false if it isn't.
     */
     bool opBinaryRight(string op: "in")(T lhs) const {
         return (lhs in set) !is null;
+    }
+
+    /**
+     * Provides the `&` operator.
+     * Params: another set (with the same item type) to intersect with.
+     * Returns: the intersection of this set and the other set.
+    */
+    auto opBinary(string op: "&")(ref const AAset!T other) const {
+        import std.range: chain;
+        AAset!T intersection;
+        foreach (item; chain(set.byKey, other.set.byKey))
+            if (item in set && item in other.set)
+                intersection.add(item);
+        return intersection;
+    }
+
+    /**
+     * Provides the `&=` operator.
+     * Params: another set (with the same item type) to intersect with.
+     * Returns: this set after it has been intersected with the other set.
+    */
+    auto opOpAssign(string op: "&")(ref const AAset!T other) {
+        import std.range: chain;
+        foreach (item; chain(set.byKey, other.set.byKey))
+            if (item !in set || item !in other.set)
+                set.remove(item);
+        return this;
+    }
+
+    /**
+     * Provides the `|` operator.
+     * Params: another set (with the same item type) to union with.
+     * Returns: the union of this set and the other set.
+    */
+    auto opBinary(string op: "|")(ref const AAset!T other) const {
+        import std.algorithm: each;
+        import std.range: chain;
+        AAset!T unioned;
+        each!(item => unioned.add(item))(chain(set.byKey, other.set.byKey));
+        return unioned;
+    }
+
+    /**
+     * Provides the `|=` operator.
+     * Params: another set (with the same item type) to union with.
+     * Returns: this set after it has been unioned with the other set.
+    */
+    auto opOpAssign(string op: "|")(ref const AAset!T other) {
+        import std.algorithm: each;
+        each!(item => add(item))(other.set.byKey);
+        return this;
     }
 
     /** The maxToStringItems controls how many items are output by
@@ -184,4 +235,24 @@ unittest {
     assert(numbers.length == 5);
     numbers.add(600, 700, 800, 100, 200);
     assert(numbers.length == 8);
+
+    auto one = AAset!int(1, 2, 3, 4, 5, 6);
+    const two = AAset!int(2, 4, 6, 8);
+    immutable three = one & two;
+    assert(three.length == 3 && 2 in three && 4 in three && 6 in three);
+    assert(one.length == 6 && 8 !in one);
+    one.add(7, 8, 9, 10);
+    assert(one.length == 10 && 8 in one);
+    one &= two;
+    assert(one.length == 4 && 2 in one && 4 in one && 6 in one && 8 in one);
+
+    auto a = AAset!int(1, 2, 3, 4, 5, 6);
+    const b = AAset!int(2, 4, 6, 8, 10);
+    immutable c = a | b;
+    assert(c.length == 8 && 1 in c && 2 in c && 3 in c && 4 in c &&
+           5 in c && 6 in c && 8 in c && 10 in c);
+    assert(a.length == 6 && 8 !in a && 10 !in a);
+    a |= b;
+    assert(a.length == 8 && 1 in a && 2 in a && 3 in a && 4 in a &&
+           5 in a && 6 in a && 8 in a && 10 in a);
 }
